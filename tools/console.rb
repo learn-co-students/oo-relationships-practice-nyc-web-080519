6b204adac9e10e138d6751a000a328fb2c4be3f4
aresnik11 <aresnik11@gmail.com>
2019-08-11 20:30:16 -0400

    finished airbnb model

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+guest1 = Guest.new("Amy")
+guest2 = Guest.new("Steven")
+guest3 = Guest.new("Howard")
+guest4 = Guest.new("Caryn")
+
+listing1 = Listing.new("NYC")
+listing2 = Listing.new("Boston")
+listing3 = Listing.new("Newport")
+listing4 = Listing.new("Nantucket")
+
+trip1 = Trip.new(listing1, guest1)
+trip2 = Trip.new(listing2, guest2)
+trip3 = Trip.new(listing3, guest3)
+trip4 = Trip.new(listing4, guest4)
+trip5 = Trip.new(listing2, guest1)
+
 Pry.start
