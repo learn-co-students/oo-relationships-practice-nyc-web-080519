d85dcfca70fc5691c3a3221c14eb89b1e29954bf
aresnik11 <aresnik11@gmail.com>
2019-08-11 21:10:21 -0400

    finished bakery model

--- tools/console.rb
+++ tools/console.rb
@@ -20,4 +20,18 @@ trip3 = Trip.new(listing3, guest3)
 trip4 = Trip.new(listing4, guest4)
 trip5 = Trip.new(listing2, guest1)
 
+bakery1 = Bakery.new("Bourke Street Bakery", "NYC")
+bakery2 = Bakery.new("Seven Grams Cafe", "NYC")
+
+dessert1 = Dessert.new("Chocolat Chip Cookie", bakery2)
+dessert2 = Dessert.new("Summer Danish", bakery1)
+dessert3 = Dessert.new("Lemon Curd Tarte", bakery1)
+
+ingredient1 = Ingredient.new("chocolate chips", 300, dessert1)
+ingredient2 = Ingredient.new("sugar", 200, dessert1)
+ingredient3 = Ingredient.new("sugar", 200, dessert2)
+ingredient4 = Ingredient.new("sugar", 200, dessert3)
+ingredient5 = Ingredient.new("lemon", 20, dessert3)
+ingredient6 = Ingredient.new("pluot", 60, dessert2)
+
 Pry.start
